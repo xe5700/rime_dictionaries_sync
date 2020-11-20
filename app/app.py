125632d29c26d8cbf57d9fb67b6d4d593096f525
xjboss <9338143+xjboss@users.noreply.github.com>
2020-11-20 15:39:38 +0800

    修复代码

--- app/app.py
+++ app/app.py
@@ -1,5 +1,4 @@
-
-from subprocess import run,Popen
+from subprocess import run, Popen
 import subprocess
 from os import path
 from crontab import CronTab
@@ -9,73 +8,82 @@ from utils import *
 from time import sleep
 import datetime
 import shutil
+
 time_format = "%Y年-%m月-%d日 %M：%S"
 
+
 def main():
-	env = os.environ
-	once = str2bool(env.get("RUN_ONCE", "False"))
-	baidu = env.get("USE_BAIDU","False")
-	sogou = env.get("USE_SOGOU","True")
-	cron_dict_update = env.get("CRON_DICT_UPDATE", "0 3 */7 * *")
-	dict_type = env.get("DICT_TYPE", "clover")
-	sogou_dir_re = env["SOGOU_DIR_RE"]
-	sogou_file_re = env["SOGOU_FILE_RE"]
-	baidu_dir_re = env["BAIDU_DIR_RE"]
-	baidu_file_re = env["BAIDU_FILE_RE"]
-	rclone = str2bool(env["USE_RCLONE"])
-	remote_config = env["REMOTE_CONFIG"]
-	remote_path = env["REMOTE_SYNC_PATH"]
-	def sync_dir_to_local():
-		print("同步文件夹到本地")
-		run(["rclone", "sync", f"{remote_config}:{remote_path}","--create-empty-src-dirs", "/remote", "-v"])
-	def sync_dir_to_remote():
-		print("同步文件夹到远程")
-		run(["rclone", "sync", "/remote", f"{remote_config}:{remote_path}","--create-empty-src-dirs", "--checksum", "-v", "--exclude","*.txt"])
-	def dl_and_sync():
-		print("开始下载并生成词库")
-		if rclone:
-			sync_dir_to_local()
-		rime_update.main()
-		if rclone:
-			for user in os.listdir("/remote"):
-				if user == "generic":
-					continue
-				fn = path.join("/remote", user)
-				if path.isdir(fn):
-					print("开始复制字典")
-					os.system(f"cp -rf /dicts/* {fn}")
-			sync_dir_to_remote()
-	print("你的设置为............")
-	print(f"字典更新类型：{dict_type}")
-	print(f"词库更新时间：{cron_dict_update}")
-	print(f"搜狗词库：{sogou}")
-	print(f"百度词库：{baidu}")
-	print(f"搜狗词库类型正则：{sogou_dir_re}")
-	print(f"搜狗词库名称正则：{sogou_file_re}")
-	print(f"百度词库类型正则：{baidu_dir_re}")
-	print(f"百度词库名称正则：{baidu_file_re}")
-	print(f"RCLONE远程同步：{rclone}")
-	print(f"远程文件夹：{remote_path}")
-	if once:
-		print("你添加了ONCE变量，将不使用计划任务只运行一次任务。")
-	if rclone:
-		print("RCLONE需要使用配置文件，检测是否存在配置文件，不存在配置文件本程序将终止工作。")
-		if not os.path.exists("/config/rclone/rclone.conf"):
-			print("你未配置RCLONE的配置文件，程序停止工作。")
-			exit(-1)
-	dl_and_sync()
-	while not once:
-		cron = CronTab(cron_dict_update, loop=True, random_seconds=True)
-		nt = datetime.datetime.now().strftime(time_format)
-		delayt = cron.next(default_utc=False)
-		nextrun = datetime.datetime.now() + datetime.timedelta(seconds=delayt)
-		nextruntime = nextrun.strftime(time_format)
-		print(f"当前时间 {nt}")
-		print(f"下一次执行时间 {nextruntime}")
-		sleep(delayt)
-		dl_and_sync()
+    env = os.environ
+    once = str2bool(env.get("RUN_ONCE", "False"))
+    baidu = env.get("USE_BAIDU", "False")
+    sogou = env.get("USE_SOGOU", "True")
+    cron_dict_update = env.get("CRON_DICT_UPDATE", "0 3 */7 * *")
+    dict_type = env.get("DICT_TYPE", "clover")
+    sogou_dir_re = env["SOGOU_DIR_RE"]
+    sogou_file_re = env["SOGOU_FILE_RE"]
+    baidu_dir_re = env["BAIDU_DIR_RE"]
+    baidu_file_re = env["BAIDU_FILE_RE"]
+    rclone = str2bool(env["USE_RCLONE"])
+    remote_config = env["REMOTE_CONFIG"]
+    remote_path = env["REMOTE_SYNC_PATH"]
+
+    def sync_dir_to_local():
+        print("同步文件夹到本地")
+        run(["rclone", "sync", f"{remote_config}:{remote_path}", "--create-empty-src-dirs", "/remote", "-v"])
+
+    def sync_dir_to_remote():
+        print("同步文件夹到远程")
+        run(["rclone", "sync", "/remote", f"{remote_config}:{remote_path}", "--create-empty-src-dirs", "--checksum",
+             "-v", "--exclude", "*.txt"])
+
+    def dl_and_sync():
+        print("开始下载并生成词库")
+        if rclone:
+            sync_dir_to_local()
+        rime_update.main()
+        if rclone:
+            for user in os.listdir("/remote"):
+                if user == "generic":
+                    continue
+                fn = path.join("/remote", user)
+                if path.isdir(fn):
+                    print("开始复制字典")
+                    os.system(f"cp -rf /dicts/* {fn}")
+            sync_dir_to_remote()
+
+    print("你的设置为............")
+    print(f"字典更新类型：{dict_type}")
+    print(f"词库更新时间：{cron_dict_update}")
+    print(f"搜狗词库：{sogou}")
+    print(f"百度词库：{baidu}")
+    print(f"搜狗词库类型正则：{sogou_dir_re}")
+    print(f"搜狗词库名称正则：{sogou_file_re}")
+    print(f"百度词库类型正则：{baidu_dir_re}")
+    print(f"百度词库名称正则：{baidu_file_re}")
+    print(f"RCLONE远程同步：{rclone}")
+    print(f"远程文件夹：{remote_path}")
+    if once:
+        print("你添加了ONCE变量，将不使用计划任务只运行一次任务。")
+    if rclone:
+        print("RCLONE需要使用配置文件，检测是否存在配置文件，不存在配置文件本程序将终止工作。")
+        if not os.path.exists("/config/rclone/rclone.conf"):
+            print("你未配置RCLONE的配置文件，程序停止工作。")
+            exit(-1)
+    dl_and_sync()
+    while not once:
+        cron = CronTab(cron_dict_update, loop=True, random_seconds=True)
+        nt = datetime.datetime.now().strftime(time_format)
+        delayt = cron.next(default_utc=False)
+        nextrun = datetime.datetime.now() + datetime.timedelta(seconds=delayt)
+        nextruntime = nextrun.strftime(time_format)
+        print(f"当前时间 {nt}")
+        print(f"下一次执行时间 {nextruntime}")
+        sleep(delayt)
+        dl_and_sync()
+
+
 if __name__ == "__main__":
-	print('''	
+    print('''	
 ______ ________  ___ _____  ______ _____ _____ _____   _   _____________  ___ _____ _____ 
 | ___ \_   _|  \/  ||  ___| |  _  \_   _/  __ \_   _| | | | | ___ \  _  \/ _ \_   _|  ___|
 | |_/ / | | | .  . || |__   | | | | | | | /  \/ | |   | | | | |_/ / | | / /_\ \| | | |__  
@@ -85,9 +93,9 @@ ______ ________  ___ _____  ______ _____ _____ _____   _   _____________  ___ __
                                               
 	''')
 
-	print("欢迎使用 中州韵 拼音词库自动更新系统")
-	print("本系统可以下载搜狗和百度的拼音的词库来为你的中州韵提供最新的词库")
-	print("并且通过同步软件将更新同步到所有的设备。")
-	print("支援朙月拼音并且支持四叶草拼音")
-	print("本脚本设计为DOCKER或者GITHUB ACTION 环境运行")
-	main()
+    print("欢迎使用 中州韵 拼音词库自动更新系统")
+    print("本系统可以下载搜狗和百度的拼音的词库来为你的中州韵提供最新的词库")
+    print("并且通过同步软件将更新同步到所有的设备。")
+    print("支援朙月拼音并且支持四叶草拼音")
+    print("本脚本设计为DOCKER或者GITHUB ACTION 环境运行")
+    main()
--- app/rime_update.py
+++ app/rime_update.py
@@ -1,43 +1,47 @@
-import os
-from os import environ, path
-from subprocess import run,Popen
-import subprocess
-from typing import List
-import shutil
-import threading
-from concurrent.futures import ThreadPoolExecutor
-import re
-import io
 import datetime
-import urllib.request
+import io
+import os
+import re
+import shutil
+import subprocess
+from concurrent.futures import ThreadPoolExecutor
+from os import path
+from subprocess import run, Popen
+from typing import List
+
 from utils import *
 
 dict_type: str
+
+
 def getfiles(p: str) -> List[str]:
-	files = []
-	for f in os.listdir(p):
-		nf = path.join(p,f)
-		if path.isdir(nf):
-			files.extend(getfiles(nf))
-		else:
-			files.append(nf)
-	return files
+    files = []
+    for f in os.listdir(p):
+        nf = path.join(p, f)
+        if path.isdir(nf):
+            files.extend(getfiles(nf))
+        else:
+            files.append(nf)
+    return files
+
 
 def findDirs(p: str) -> List[str]:
-	files = []
-	for f in os.listdir(p):
-		nf = path.join(p,f)
-		if path.isdir(nf):
-			files.append(nf)
-			files.extend(findDirs(nf))
-	return files
+    files = []
+    for f in os.listdir(p):
+        nf = path.join(p, f)
+        if path.isdir(nf):
+            files.append(nf)
+            files.extend(findDirs(nf))
+    return files
+
 
 def getnametype(p: str):
-	p = p[:p.rfind(".")]
-	t = "/tmp/dicts/"
-	if p.startswith(t):
-		return p[len(t):]
-	
+    p = p[:p.rfind(".")]
+    t = "/tmp/dicts/"
+    if p.startswith(t):
+        return p[len(t):]
+
+
 yaml_header_group = '''# Rime dictionary
 # encoding: utf-8
 #d
@@ -82,131 +86,139 @@ version: "{ver}"
 '''
 convert: str
 invalidfns = re.compile('【|】|（|）|！|※|《|》| ')
+
+
 def convert_file(file_name: str):
-		cv2 = convert.format(i=file_name, type="{type}")
-		if file_name.endswith(".scel"):
-			cv2 = cv2.format(type="scel")
-		elif file_name.endswith(".qpyd"):
-			cv2 = cv2.format(type="qpyd")
-		elif file_name.endswith(".qcel"):
-			cv2 = cv2.format(type="qcel")
-		elif file_name.endswith(".bdict"):
-			cv2 = cv2.format(type="bdict")
-		elif file_name.endswith(".sgpy"):
-			cv2 = cv2.format(type="sgpy")
-		elif file_name.endswith(".sgpybin"):
-			cv2 = cv2.format(type="sgpybin")
-		elif file_name.endswith(".bdpy"):
-			cv2 = cv2.format(type="bdpy")
-		run(cv2, shell=True, stdout=subprocess.DEVNULL)
-		ndire_name = ""
-		def luna():
-			global ndire_name
-			fn_pipe = Popen(f"echo {file_name} | opencc -c s2t.json", stdout=subprocess.PIPE)
-			fn_pipe.wait()
-			fn_name = fn_pipe.stdout.read()
-			dire_type = "luna."+getnametype(fn_name).replace('/','.')
-			dire_type = invalidfns.sub('_',dire_type)
-			ndire_name = dire_type+".dict.yaml"
-			# 朙月拼音需要转换为繁体才可以使用
-			run(["opencc","-i","/tmp/rime.txt","-o","/tmp/rime2.txt","-c","s2t.json"])
-			with open("/tmp/rime2.txt", "r") as f:
-				dict_info = f.read()
-				with open("/tmp/dicts_out/"+ndire_name, "w+") as f2:
-					data = yaml_header.format(name=dire_type, ver=datetime.datetime.fromtimestamp(os.path.getmtime(file_name)).isoformat())
-					data += dict_info
-					f2.write(data)
-					pass
-			os.remove("/tmp/rime.txt")
-			os.remove("/tmp/rime2.txt")
-		def clover():
-			global ndire_name
-			dire_type = getnametype(file_name).replace('/','.')
-			dire_type = invalidfns.sub('_',dire_type)
-			ndire_name = "clover."+dire_type+".dict.yaml"
-			with open("/tmp/rime.txt", "r") as f:
-				dict_info = f.read()
-				with open("/tmp/dicts_out/"+ndire_name, "w+") as f2:
-					data = yaml_header.format(name=dire_type, ver=datetime.datetime.fromtimestamp(os.path.getmtime(file_name)).isoformat())
-					data += dict_info
-					f2.write(data)
-					pass
-			os.remove("/tmp/rime.txt")
-		# 朙月拼音
-		if dict_type == "luna":
-			luna()
-		# 四叶草拼音
-		elif dict_type == "clover":
-			clover()
-		elif dict_type == "all":
-			luna()
-			clover()
-		print("成功转换 "+ ndire_name)
+    cv2 = convert.format(i=file_name, type="{type}")
+    if file_name.endswith(".scel"):
+        cv2 = cv2.format(type="scel")
+    elif file_name.endswith(".qpyd"):
+        cv2 = cv2.format(type="qpyd")
+    elif file_name.endswith(".qcel"):
+        cv2 = cv2.format(type="qcel")
+    elif file_name.endswith(".bdict"):
+        cv2 = cv2.format(type="bdict")
+    elif file_name.endswith(".sgpy"):
+        cv2 = cv2.format(type="sgpy")
+    elif file_name.endswith(".sgpybin"):
+        cv2 = cv2.format(type="sgpybin")
+    elif file_name.endswith(".bdpy"):
+        cv2 = cv2.format(type="bdpy")
+    run(cv2, shell=True, stdout=subprocess.DEVNULL)
+    ndire_name = ""
+
+    def process_default():
+        os.rename("/tmp/rime.txt", "/tmp/rime2.txt")
+
+    # noinspection PyGlobalUndefined
+    def process_1(typen: str, func):
+        global ndire_name
+        fn_pipe = Popen(f"echo {file_name} | opencc -c s2t.json", shell=True, stdout=subprocess.PIPE)
+        fn_pipe.wait()
+        fn_name = fn_pipe.stdout.read().decode()
+        dire_type = f"{typen}." + getnametype(fn_name).replace('/', '.')
+        dire_type = invalidfns.sub('_', dire_type)
+        ndire_name = dire_type + ".dict.yaml"
+        func()
+        with open("/tmp/rime2.txt", "r") as f:
+            dict_info = f.read()
+            with open("/tmp/dicts_out/" + ndire_name, "w+") as f2:
+                data = yaml_header.format(name=dire_type,
+                                          ver=datetime.datetime.fromtimestamp(os.path.getmtime(file_name)).isoformat())
+                data += dict_info
+                f2.write(data)
+                pass
+        os.remove("/tmp/rime.txt")
+        os.remove("/tmp/rime2.txt")
+
+    # noinspection PyTypeChecker
+    def luna():
+        process_1("luna", lambda: run(["opencc", "-i", "/tmp/rime.txt", "-o", "/tmp/rime2.txt", "-c", "s2t.json"]))
+        # 朙月拼音需要转换为繁体才可以使用
+
+    def clover():
+        process_1("clover", process_default)
+
+    # 朙月拼音
+    if dict_type == "luna":
+        luna()
+    # 四叶草拼音
+    elif dict_type == "clover":
+        clover()
+    elif dict_type == "all":
+        luna()
+        clover()
+    print("成功转换 " + ndire_name)
+
 
 def main():
-	global convert, dict_type
-	dict_type = os.environ.get("DICT_TYPE","clover")
-	d_baidu: Popen = None
-	d_sogou: Popen = None
-	if str2bool(os.environ["USE_SOGOU"]):
-		print("正在下载搜狗词库")
-		d_sogou = Popen(["python2", "ThesaurusSpider/SougouThesaurusSpider/multiThreadDownload.py"])
-	if str2bool(os.environ["USE_BAIDU"]):
-		print("正在下载百度词库")
-		d_baidu = Popen(["python2", "ThesaurusSpider/BaiduTheaurusSpider/multiThreadDownload.py"])
-	#print("正在下载QQ词库")
-	#d_qq = Popen(["python2", "ThesaurusSpider/QQTheaurusSpider/multiThreadDownload.py"])
-	d_sogou: subprocess.Popen
-	d_baidu: subprocess.Popen
-	if d_sogou != None:
-		d_sogou.wait()
-		
-	if d_baidu != None:
-		d_baidu.wait()
+    global convert, dict_type
+    dict_type = os.environ.get("DICT_TYPE", "clover")
+    d_baidu: Popen = None
+    d_sogou: Popen = None
+    if str2bool(os.environ["USE_SOGOU"]):
+        print("正在下载搜狗词库")
+        d_sogou = Popen(["python2", "ThesaurusSpider/SougouThesaurusSpider/multiThreadDownload.py"])
+    if str2bool(os.environ["USE_BAIDU"]):
+        print("正在下载百度词库")
+        d_baidu = Popen(["python2", "ThesaurusSpider/BaiduTheaurusSpider/multiThreadDownload.py"])
+    # print("正在下载QQ词库")
+    # d_qq = Popen(["python2", "ThesaurusSpider/QQTheaurusSpider/multiThreadDownload.py"])
+    d_sogou: subprocess.Popen
+    d_baidu: subprocess.Popen
+    if d_sogou != None:
+        d_sogou.wait()
 
-	#d_qq.wait()
-	print("查找词库")
-	alldires = getfiles("/tmp/dicts/")
-	#alldires_type = findDirs("/tmp/dicts/")
-	convert = "dotnet ./imewlconverter/ImeWlConverterCmd.dll -i:{type} '{i}' -o:rime /tmp/rime.txt"
-	print("开始转换词库 为 中州韵格式")
-	os.mkdir('/tmp/dicts_out/')
-	pool = ThreadPoolExecutor(max_workers=4)
-	for dire in alldires:
-		pool.submit(convert_file, dire)
-	pool.shutdown()
-	
-	def mk_all(alldicts: List[str],type: str):
-			with open(f"/tmp/dicts_out/{type}.autoupdate.dict.yaml", "w+") as of:
-				groupsx = io.StringIO()
-				for f in alldicts:
-					fl=f.rfind(".dict.yaml")
-					f=f[:fl]
-					groupsx.write(f"   - {f}\n")
-				out = yaml_header_group.format(name=f"{type}.autoupdate",imports=groupsx.getvalue(), ver=datetime.datetime.now().isoformat())
-				of.write(out)
-				print(out)
-				print("成功输出词库集 "+of.name)
-			os.system('mv /tmp/dicts_out/* /dicts/')
-	alldicts = os.listdir("/tmp/dicts_out")
-	dicts_luna = []
-	dicts_clover = []
-	for i in alldicts:
-		if i.startswith("clover."):
-			dicts_clover.append(i)
-		elif i.startswith("luna."):
-			dicts_luna.append(i)
-	if len(dicts_luna) > 0:
-		mk_all(dicts_luna, "luna")
-	if len(dicts_clover) > 0:
-		mk_all(dicts_clover, "clover")
-	print("执行清理")
-	for f in os.listdir("/tmp/"):
-		fn = f"/tmp/{f}"
-		print(f"清理 {fn}")
-		if path.isdir(fn):
-			shutil.rmtree(fn)
-		else:
-			os.remove(fn)
+    if d_baidu != None:
+        d_baidu.wait()
+
+    # d_qq.wait()
+    print("查找词库")
+    alldires = getfiles("/tmp/dicts/")
+    # alldires_type = findDirs("/tmp/dicts/")
+    convert = "dotnet ./imewlconverter/ImeWlConverterCmd.dll -i:{type} '{i}' -o:rime /tmp/rime.txt"
+    print("开始转换词库 为 中州韵格式")
+    os.mkdir('/tmp/dicts_out/')
+    pool = ThreadPoolExecutor(max_workers=4)
+    for dire in alldires:
+        pool.submit(convert_file, dire)
+    pool.shutdown()
+
+    def mk_all(alldicts: List[str], type: str):
+        with open(f"/tmp/dicts_out/{type}.autoupdate.dict.yaml", "w+") as of:
+            groupsx = io.StringIO()
+            for f in alldicts:
+                fl = f.rfind(".dict.yaml")
+                f = f[:fl]
+                groupsx.write(f"   - {f}\n")
+            out = yaml_header_group.format(name=f"{type}.autoupdate", imports=groupsx.getvalue(),
+                                           ver=datetime.datetime.now().isoformat())
+            of.write(out)
+            print(out)
+            print("成功输出词库集 " + of.name)
+        os.system('mv /tmp/dicts_out/* /dicts/')
+
+    alldicts = os.listdir("/tmp/dicts_out")
+    dicts_luna = []
+    dicts_clover = []
+    for i in alldicts:
+        if i.startswith("clover."):
+            dicts_clover.append(i)
+        elif i.startswith("luna."):
+            dicts_luna.append(i)
+    if len(dicts_luna) > 0:
+        mk_all(dicts_luna, "luna")
+    if len(dicts_clover) > 0:
+        mk_all(dicts_clover, "clover")
+    print("执行清理")
+    for f in os.listdir("/tmp/"):
+        fn = f"/tmp/{f}"
+        print(f"清理 {fn}")
+        if path.isdir(fn):
+            shutil.rmtree(fn)
+        else:
+            os.remove(fn)
+
+
 if __name__ == "__main__":
-	main()
+    main()
